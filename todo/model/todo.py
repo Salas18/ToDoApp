4886b7ef1510603be4c19ffd3544ac5f032e16a4
Sebastian Hernandez <jsebasha2001@gamil.com>
2025-03-11 13:24:46 -0500

    se agrega el metodo mark_completed

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,10 +1,30 @@
 class Todo:
+    mark_completed: True
     def __init__(self, code_id : int, title: str, description: str):
         self.code_id: int = code_id
         self.title: str = title
         self.description: str = description
         self.completed: bool = False
         self.tags: list[str] = []
+
+    def mark_completed(self, completed : bool = True):
+        return completed
+
+    def add_tagg(self, tag: str):
+        for tag in self.tags:
+            if tag is not self.tags:
+                self.tags.append(tag)
+
+    def __str__(self,):
+
+
+
+
+
+
+
+
+
 
 
 
